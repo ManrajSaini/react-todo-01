19871f4909fb05318118dc5e98cbd99698075746
Manraj Singh <manraj.saini123@gmail.com>
2023-10-25 11:33:32 +0530

    creating todo done

---
+++ src/TodoList.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {v4 as uuidv4} from "uuid";
+import { useState } from 'react';
+
+const TodoList = () => {
+
+    const [inputTask, setInputTask] = useState("");
+    const [list, setList] = useState([]);
+
+    const handleInputChange = (event) => {
+        setInputTask(event.target.value);
+    };
+
+    const handleAddTodo = (task) => {
+        const newTask = {
+            id: uuidv4(),
+            todo: task
+        };
+
+        setList([...list, newTask]);
+        setInputTask("");
+    }
+
+    return (
+        <div className='Todo'>
+            <h1>My Todo-List</h1>
+            <div className='Top'>
+                <input 
+                    className='input' 
+                    type='text' 
+                    value={inputTask} 
+                    onChange={handleInputChange} 
+                    placeholder='Enter your task'
+                />
+
+                <button 
+                    className='btn' 
+                    onClick={() => handleAddTodo(inputTask)}
+                >ADD</button>
+            </div>
+
+            <ul>
+                {list.map((task) => (
+                    <li className='task' key={task.id}> 
+                        {task.todo}
+                    
+                    </li>
+                ))}
+            </ul>
+
+        </div>
+    )
+}
+
+export default TodoList;
